41cd31f3e73b3b7435a791ba3f21ee4ae03dc627
Phạm Đức Đạt <91515103+Dominic2k@users.noreply.github.com>
2024-09-19 12:26:56 +0700

    update

--- day_9_js.js
+++ day_9_js.js
@@ -40,15 +40,105 @@ const createMultiplier = factor => {
 //console.log(createMultiplier(5)(2));  // Output: 10
 //console.log(createMultiplier(5)(3));  // Output: 15
 
-const numbers = [1, 2, 3, 4, 5];
-const sumArray = arr => {
-    let sum = 0;
-    // const callback = (element) => {
-    //     sum += element;
-    // }
-    arr.forEach(element => {
-        sum += element;
-    });
-    return sum;
-}
-console.log(sumArray(numbers));
+// const numbers = [1, 2, 3, 4, 5];
+// const sumArray = arr => {
+//     let sum = 0;
+//     // const callback = (element) => {
+//     //     sum += element;
+//     // }
+//     arr.forEach(element => {
+//         sum += element;
+//     });
+//     return sum;
+// }
+// console.log(sumArray(numbers));\
+
+
+// const numbers = [1, 2, 3, 4];
+
+// const sumArray = arr => {
+//   let sum = 0;
+//   arr.forEach(function(element) {
+//     sum += element;
+//   })
+//   return sum;
+// }
+// console.log(sumArray(numbers));
+
+// function sayHello() {
+//     console.log('Hello');
+// }
+// setInterval(sayHello, 1000); // it prints hello in every second, 1000ms is 1s
+
+// function sayHello() {
+//     console.log('Hello');
+// }
+// setTimeout(sayHello, 2000); // it prints hello after it waits for 2 seconds.
+
+// let arr = [1,2,3,4,5,6,7,8];
+
+// arr.forEach(function (element, index, arr) {
+//     console.log(element,index, arr);
+// })
+//   // The above code can be written using arrow function
+// arr.forEach((element, index, arr) => {
+//     console.log(element,index, arr);
+// })
+//   // The above code can be written using arrow function and explicit return
+// arr.forEach((element, index, arr) => console.log(index, element, arr));
+
+// let sum = 0;
+// const numbers = [1, 2, 3, 4, 5];
+// numbers.forEach((number) => {
+// 	sum += number;
+// 	console.log(number);
+// 	console.log(sum);
+// });
+
+
+/*Arrow function and explicit return
+const modifiedArray = arr.map((element,index) => element);
+*/
+//Example
+// const numbers = [1, 2, 3, 4, 5];
+// const numbersSquare = numbers.map((num) => num * num + 1);
+
+// console.log(numbersSquare);
+// console.log(numbers);
+
+
+// const countries = [
+// 	'Albania',
+// 	'Bolivia',
+// 	'Canada',
+// 	'Denmark',
+// 	'Ethiopia',
+// 	'Finland',
+// 	'Germany',
+// 	'Hungary',
+// 	'Ireland',
+// 	'Japan',
+// 	'Kenya',
+//   ]
+// const countriesToUpperCase1 = countries.map((country) => {
+// 	return country.toUpperCase();
+// });
+// console.log(countriesToUpperCase1);
+
+// const countriesToUpperCase2 = countries.map((country) => country.toUpperCase());
+
+// console.log(countriesToUpperCase2);
+
+  
+  /*
+  // Arrow function
+  const countriesToUpperCase = countries.map((country) => {
+	return country.toUpperCase();
+  })
+  //Explicit return arrow function
+  const countriesToUpperCase = countries.map(country => country.toUpperCase());
+  */
+
+const countriesFirstThreeLetters = countries.map((country) =>
+	country.toUpperCase().slice(0, 3)
+)
