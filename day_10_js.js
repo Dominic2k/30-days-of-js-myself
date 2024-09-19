5b0e7e4f424ce29b725365116443600a3170209a
Phạm Đức Đạt <91515103+Dominic2k@users.noreply.github.com>
2024-09-19 21:40:07 +0700

    update day 10

---
+++ day_10_js.js
@@ -0,0 +1,76 @@
+
+// const companies = new Set();
+// console.log(companies);
+
+// const languages = [
+// 	'English',
+// 	'Finnish',
+// 	'English',
+// 	'French',
+// 	'Spanish',
+// 	'English',
+// 	'French',
+// ];
+  
+// const setOfLanguages = new Set(languages);
+// console.log(setOfLanguages);
+
+// const languages = [
+// 	'English',
+// 	'Finnish',
+// 	'English',
+// 	'French',
+// 	'Spanish',
+// 	'English',
+// 	'French',
+// ];
+  
+// const setOfLanguages = new Set(languages);
+  
+// for (let language of setOfLanguages) {
+// 	console.log(language);
+// };
+
+
+// let a = [1, 2, 3, 4, 5];
+// let b = [3, 4, 5, 6];
+// let c = [...a, ...b];
+
+// let A = new Set(a);
+// let B = new Set(b);
+// let C = new Set(c);
+
+// console.log(C);
+
+// let a = [1, 2, 3, 4, 5];
+// let b = [3, 4, 5, 6];
+
+// let A = new Set(a);
+// let B = new Set(b);
+
+// let c = a.filter((num) => 
+// 	B.has(num));
+// let C = new Set(c);
+
+// console.log(C);
+
+// let a = [1, 2, 3, 4, 5];
+// let b = [3, 4, 5, 6];
+
+// let A = new Set(a);
+// let B = new Set(b);
+
+// let c = a.filter((num) => !B.has(num));
+
+// let C = new Set(c);
+
+// console.log(C);
+
+countries = [
+	['Finland', 'Helsinki'],
+	['Sweden', 'Stockholm'],
+	['Norway', 'Oslo']
+];
+const map = new Map(countries);
+console.log(map);
+console.log(map.size);
--- day_9_js.js
+++ day_9_js.js
@@ -170,11 +170,68 @@ const modifiedArray = arr.map((element,index) => element);
 
 // console.log(sum);
 
-const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook'];
-const areAllStr = names.every((name) => typeof name === 'string'); // Are all strings?
+// const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook'];
+// const areAllStr = names.every((name) => typeof name === 'string'); // Are all strings?
 
-console.log(areAllStr);
+// console.log(areAllStr);
 
 
+// const products = ['Milk', 'Coffee', 'Sugar', 'Honey', 'Apple', 'Carrot'];
+// console.log(products.sort()) // ['Apple', 'Carrot', 'Coffee', 'Honey', 'Milk', 'Sugar']
+// //Now the original products array is also sorted
 
+// const numbers = [9.81, 3.14, 100, 37];
+// // Using sort method to sort number items provide a wrong result. see below
+// console.log(numbers.sort()); //[100, 3.14, 37, 9.81]
+// numbers.sort((a, b) => {
+//   return a - b;
+// })
+
+// console.log(numbers) // [3.14, 9.81, 37, 100]
+
+// numbers.sort((a, b) => {
+//   return b - a;
+// })
+// console.log(numbers); //[100, 37, 9.81, 3.14]
+
+// objArr.sort(function (a, b) {
+// 	if (a.key < b.key) return -1;
+// 	if (a.key > b.key) return 1;
+// 	return 0;
+// })
+  
+//   // or
+  
+// objArr.sort(function (a, b) {
+// 	if (a['key'] < b['key']) return -1;
+// 	if (a['key'] > b['key']) return 1;
+// 	return 0;
+// })
+  
+// const users = [
+// 	{ name: 'Asabeneh', age: 150 },
+// 	{ name: 'Brook', age: 50 },
+// 	{ name: 'Eyob', age: 100 },
+// 	{ name: 'Elias', age: 22 },
+// ];
+// users.sort((a,b) => {
+// 	if (a.age < b.age) return -1
+//   	if (a.age > b.age) return 1
+//   	return 0
+// });
+
+// users.sort((a,b) => {
+// 	if (a.age < b.age) {
+// 		return -1;
+// 	}
+// 	if (a.age > b.age) {
+// 		return 1;
+// 	}
+// 	return 0;
+// });
+			
+// console.log(users);
+
+// sorted ascending
+  // [{…}, {…}, {…}, {…}]
 
